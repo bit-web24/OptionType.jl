3dd419edfbb9268236261260691b8670c2bc9973
bit-web24 <bitweb24@gmail.com>
2023-07-09 01:27:29 +0530

    Option{T} type

--- src/Options.jl
+++ src/Options.jl
@@ -1,5 +1,55 @@
 module Options
 
-# Write your package code here.
+export Option, None, Some, isempty, get, map, and_then, or_else
+
+struct Option{T}
+    value::Union{T, Nothing}
+end
+
+const None = Option{Any}(nothing)
+
+function Some(x::T) where {T}
+    Option(x)
+end
+
+# Function to check if an option is empty
+function isempty(option::Option)
+    option.value === nothing
+end
+
+# Function to get the value of an Option or return a default value
+function get(option::Option{T}, default::T) where {T}
+    isempty(option) ? default : option.value
+end
+
+# Function to get the value of an Option or panic with a custom error message
+function get(option::Option{T}, error_message::AbstractString) where {T}
+    isempty(option) ? error(error_message) : option.value
+end
+
+# Function to transform an Option using a provided function
+function map(f::Function, option::Option)
+    isempty(option) ? None : Some(f(option.value))
+end
+
+# Function to apply a function to an Option, returning the result or a default value
+function and_then(option::Option, f::Function, default::Any)
+    isempty(option) ? default : f(option.value)
+end
+
+# Function to apply a function to an Option, returning the result or a provided Option
+function and_then(option::Option, f::Function, default::Option)
+    isempty(option) ? default : f(option.value)
+end
+
+# Function to apply a function to an Option, returning the result or None
+function and_then(option::Option, f::Function)
+    and_then(option, f, None)
+end
+
+# Function to apply a function to an Option, returning the result or None
+function or_else(option::Option, f::Function)
+    isempty(option) ? f() : option.value
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,53 @@
 using Options
 using Test
 
-@testset "Options.jl" begin
-    # Write your tests here.
+@testset "Some Tests" begin
+    x = Options.Some(10)
+    y = Options.Some("Hello")
+
+    @test Options.isempty(x) == false
+    @test Options.isempty(y) == false
+
+    @test Options.get(x, 0) == 10
+    @test Options.get(y, "Default") == "Hello"
+    @test Options.get(Options.map(x -> x^2, x), 0) == 100
+    @test Options.get(Options.map(s -> uppercase(s), y), "Default") == "HELLO"
+end
+
+@testset "None Tests" begin
+    n = Options.None
+
+    @test Options.isempty(n) == true
+
+    @test Options.get(n, 0) == 0
+    @test Options.get(Options.map(x -> x^2, n), 0) == 0
+end
+
+@testset "and_then Tests" begin
+    x = Options.Some(5)
+    y = Options.None
+
+    add_one = x -> x + 1
+    double = x -> x * 2
+
+    @test Options.and_then(x, add_one, 0) == 6
+    @test Options.and_then(x, double, 0) == 10
+    @test Options.and_then(x, add_one, Options.None) == 6
+    @test Options.and_then(x, double, Options.None) == 10
+    @test Options.and_then(y, add_one, 0) == 0
+    @test Options.and_then(y, double, 0) == 0
+    @test Options.and_then(y, add_one, Options.None) == Options.None
+    @test Options.and_then(y, double, Options.None) == Options.None
+    @test Options.and_then(x, add_one) == 6
+    @test Options.and_then(y, add_one) == Options.None
+end
+
+@testset "or_else Tests" begin
+    x = Options.Some(5)
+    y = Options.None
+
+    fallback = () -> 10
+
+    @test Options.or_else(x, fallback) == 5
+    @test Options.or_else(y, fallback) == 10
 end
